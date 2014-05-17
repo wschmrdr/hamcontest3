9221c40f323520c7581a0740b5e07a61a0545114
will <will@linda.(none)>
2014-05-17 08:13:49 -0400

    Changes 20140517_1214

--- scripts/contest.js
+++ scripts/contest.js
@@ -2,4 +2,28 @@ $(document).ready( function() {
     var contestList = $.parseJSON(getCookie('contestList'));
     if (!$("#title").html())
         $("#title").html(contestList['contest_name']);
+    if (!$("#score").html())
+    {
+    }
+    if (!$("#dataEntry").html())
+    {
+    }
+    if (!$("#bandSelect").html())
+    {
+    }
+    if (!$("#modeSelect").html())
+    {
+    }
+    if (!$("#sectSelect").html())
+    {
+    }
+    if (!$("#dupeArea").html())
+    {
+    }
+    if (!$("#contactList").html())
+    {
+    }
+    if (!$("#sentData").html())
+    {
+    }
 });
