a14b5e9c89880c0a2f150e1ed134b9abe7080dee
will <will@linda.(none)>
2014-05-26 21:20:15 -0400

    Changes 20140527_0120

--- sql/import_commands.sh
+++ sql/import_commands.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+COLUMN_NAMES=`cat $1 | head -n 1`
 mysqlimport -p -u root -c unique_name,long_name,short_name,data_type,enum1,enum2,enum3,max_length,double_entry,sent_data --fields_terminated_by="," --fields_enclosed_by=\" --ignore-lines=1 hamcontest data_type.csv
 mysqlimport -p -u root -c enum_type,shortname,longname --fields_terminated_by="," --fields_enclosed_by=\" --ignore-lines=1 hamcontest enum_values.csv
-mysqlimport -p -u root -c contest_name,type_data1,type_data2,type_data3,type_data4,type_data5,freq_dupe_flag,mode_dupe_flag,data1_dupe_flag,data2_dupe_flag,data3_dupe_flag,data4_dupe_flag,data5_dupe_flag,assisted_flag,band_flag,mode_flag,operator_flag,power_flag,station_flag,time_flag,transmitter_flag,overlay_flag,personal_flag --fields_terminated_by="," --fields_enclosed_by=\" --ignore-lines=1 hamcontest contest_list.csv
+mysqlimport -p -u root -c contest_name,type_data1,type_data2,type_data3,type_data4,type_data5,call_loc,freq_dupe_flag,mode_dupe_flag,data1_dupe_flag,data2_dupe_flag,data3_dupe_flag,data4_dupe_flag,data5_dupe_flag,assisted_flag,band_flag,mode_flag,operator_flag,power_flag,station_flag,time_flag,transmitter_flag,overlay_flag,personal_flag --fields_terminated_by="," --fields_enclosed_by=\" --ignore-lines=1 hamcontest contest_list.csv
