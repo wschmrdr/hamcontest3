742155c4920be18c878004493f2370b3f774f039
will <will@linda.(none)>
2014-06-02 21:44:40 -0400

    Changes 20140603_0144

--- handlers/contact_data.php
+++ handlers/contact_data.php
@@ -15,3 +15,27 @@
         }
         echo json_encode($rows);
     }
+    if (isset($_POST['contest_id']) && !empty($_GET['contest_id']))
+    {
+        session_start();
+        include('../../config/db.php');
+
+        $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $db_connection->set_charset("utf8");
+        $data = array();
+        $data["contest_id"] = $_POST['contest_id'];
+        $data["frequency"] = $_POST['frequency'];
+        $data["contactmode"] = $_POST['contactmode'];
+        $data["sentcall"] = $_POST['sentcall'];
+        $data["sentdata1"] = $_POST['sentdata1'];
+        $data["sentdata2"] = $_POST['sentdata2'];
+        $data["sentdata3"] = $_POST['sentdata3'];
+        $data["sentdata4"] = $_POST['sentdata4'];
+        $data["sentdata5"] = $_POST['sentdata5'];
+        $data["recvcall"] = $_POST['recvcall'];
+        $data["recvdata1"] = $_POST['recvdata1'];
+        $data["recvdata2"] = $_POST['recvdata2'];
+        $data["recvdata3"] = $_POST['recvdata3'];
+        $data["recvdata4"] = $_POST['recvdata4'];
+        $data["recvdata5"] = $_POST['recvdata5'];
+    }
