90d03aebd85245dcb17b263800f5946da202e2de
wschmrdr <wschmrdr@gmail.com>
2014-09-24 21:17:48 -0400

    003 - Combine SQL Data and update Readme files

--- sql/02-data_type.sql
+++ sql/02-data_type.sql
@@ -12,3 +12,14 @@ CREATE TABLE data_type (
     sent_data char(1),
     PRIMARY KEY (data_type_id)
 );
+
+INSERT INTO data_type(
+unique_name,long_name,short_name,data_type,enum1,enum2,enum3,max_length,double_entry,sent_data
+) VALUES
+('Record Number - Unlimited Digits','Record Number','NR','number','','','',0,0,0),
+('Precedent - ARRL November Sweepstakes','Precedent','Prec','special','','','',1,0,1),
+('Check - First Year Licensed - 2 Digits','Check','Chk','number','','','',2,0,1),
+('Section - ARRL/RAC and DX,Section','Sec','enum','ARRLRAC','','DX',3,0,1),
+('Exchange - ARRL Field Day,Exchange','Exc','string','','','',3,0,1),
+('Signal Report','Signal Report','RST','number','','','',3,1,0),
+('Section - New York State,Section','Sec','enum','NewYork','USCan','DX',3,0,1);
